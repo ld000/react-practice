ee298e8a618a37edc916846d2a74ae9eadc1a794
LD00000 <lidong9144@163.com>
2017-02-25 00:32:30 +0800

    webpack

--- config/project.config.js
+++ config/project.config.js
@@ -1,18 +1,25 @@
+/* eslint key-spacing:0 spaced-comment:0 */
 const path = require('path')
+const debug = require('debug')('app:config:project')
+const argv = require('yargs').argv
 const ip = require('ip')
 
+debug('Creating default configuration.')
+// ========================================================
+// Default Configuration
+// ========================================================
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env : process.env.NODE_ENV || 'development',
 
   // ----------------------------------
   // Project Structure
   // ----------------------------------
-  path_base: path.resolve(__dirname, '..'),
-  dir_client: 'src',
-  dir_dist: 'dist',
-  dir_public: 'public',
-  dir_server: 'server',
-  dir_test: 'tests',
+  path_base  : path.resolve(__dirname, '..'),
+  dir_client : 'src',
+  dir_dist   : 'dist',
+  dir_public : 'public',
+  dir_server : 'server',
+  dir_test   : 'tests',
 
   // ----------------------------------
   // Server Configuration
@@ -43,9 +50,30 @@ const config = {
     'react-redux',
     'react-router',
     'redux'
+  ],
+
+  // ----------------------------------
+  // Test Configuration
+  // ----------------------------------
+  coverage_reporters : [
+    { type : 'text-summary' },
+    { type : 'lcov', dir : 'coverage' }
   ]
 }
 
+/************************************************
+-------------------------------------------------
+
+All Internal Configuration Below
+Edit at Your Own Risk
+
+-------------------------------------------------
+************************************************/
+
+// ------------------------------------
+// Environment
+// ------------------------------------
+// N.B.: globals added here must _also_ be added to .eslintrc
 config.globals = {
   'process.env'  : {
     'NODE_ENV' : JSON.stringify(config.env)
@@ -54,15 +82,29 @@ config.globals = {
   '__DEV__'      : config.env === 'development',
   '__PROD__'     : config.env === 'production',
   '__TEST__'     : config.env === 'test',
-  // '__COVERAGE__' : !argv.watch && config.env === 'test',
+  '__COVERAGE__' : !argv.watch && config.env === 'test',
   '__BASENAME__' : JSON.stringify(process.env.BASENAME || '')
 }
 
-/**
- * 拼装 config.path_base 和 bind(null, arguments), 生成新路径
- *
- * @return {string} path
- */
+// ------------------------------------
+// Validate Vendor Dependencies
+// ------------------------------------
+const pkg = require('../package.json')
+
+config.compiler_vendors = config.compiler_vendors
+  .filter((dep) => {
+    if (pkg.dependencies[dep]) return true
+
+    debug(
+      `Package "${dep}" was not found as an npm dependency in package.json; ` +
+      `it won't be included in the webpack vendor bundle.
+       Consider removing it from \`compiler_vendors\` in ~/config/index.js`
+    )
+  })
+
+// ------------------------------------
+// Utilities
+// ------------------------------------
 function base () {
   const args = [config.path_base].concat([].slice.call(arguments))
   return path.resolve.apply(path, args)
@@ -75,5 +117,17 @@ config.paths = {
   dist   : base.bind(null, config.dir_dist)
 }
 
+// ========================================================
+// Environment Configuration
+// ========================================================
+debug(`Looking for environment overrides for NODE_ENV "${config.env}".`)
+const environments = require('./environments.config')
+const overrides = environments[config.env]
+if (overrides) {
+  debug('Found overrides, applying to default configuration.')
+  Object.assign(config, overrides(config))
+} else {
+  debug('No environment overrides found, defaults will be used.')
+}
 
 module.exports = config
